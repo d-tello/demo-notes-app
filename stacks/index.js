b6c7d87ccb3581977a1cd2e5387b59be98983d6b
Davide Tello <tello.davide@gmail.com>
2023-01-26 12:34:03 +0100

    Add new stack to the app

--- stacks/index.js
+++ stacks/index.js
@@ -1,10 +1,6 @@
-import { MyStack } from "./MyStack";
-import { App } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
 
-/**
- * @param {App} app
- */
-export default function (app) {
+ const main = (app) => {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
     srcPath: "services",
@@ -12,5 +8,7 @@ export default function (app) {
       format: "esm",
     },
   });
-  app.stack(MyStack);
+  app.stack(StorageStack);
 }
+
+export default main;
