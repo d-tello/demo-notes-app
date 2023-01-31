37f4e43d73835e0fe90ae8ad5fb982f921e59ab5
Davide Tello <tello.davide@gmail.com>
2023-01-31 10:27:29 +0100

    Building our React app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -14,6 +14,8 @@ const config = {
       APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
       IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
     },
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_51MVvjQE05Z3Xa8h6id0mVjKyFylyp76kBGb7lF9eQ78brXmrAcTy9m7tZZxuqIl4qt70H4i1mRM9gdALBmhmAjTQ009gmjl7Vl",
   };
   
   export default config;
